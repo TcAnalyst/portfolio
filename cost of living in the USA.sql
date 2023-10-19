f4457718df1342932ff9b85914b0864d89c751b9
TcAnalyst <142181097+TcAnalyst@users.noreply.github.com>
2023-10-19 20:26:30 +0100

    Update cost of living in the USA.sql

--- cost of living in the USA.sql
+++ cost of living in the USA.sql
@@ -180,3 +180,30 @@ select distinct county, median_family_income, total_cost, median_family_income-t
 FROM cost_of_living_us
 where  median_family_income-total_cost >= 0.2*median_family_income and (no_of_children + no_of_parents)=6
 order by 20_PercentOfIncome desc;
+
+
+
+
+
+
+MIT License
+
+Copyright (c) 2023 TcAnalyst
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
